443c401f2b6e554f659d0abada8f298e008c1db7
Cedric <c.beyls@gmail.com>
2016-09-12 23:53:01 +0200

    v0.0.0-alpha6

--- js/agency.js
+++ js/agency.js
@@ -1,5 +1,37 @@
-// Agency Theme JavaScript
+function initMap() {
 
+    //var myLatLng = {lat: -21.115141, lng: 55.536384};
+
+    // Create a map object and specify the DOM element for display.
+    var map = new google.maps.Map(document.getElementById('map'), {
+      center: {lat: -21.115141, lng: 55.536384},
+      scrollwheel: false,
+      zoom: 10 
+    });
+    
+    $.getJSON("locations.json", function(json1) {
+      $.each(json1, function(key, data) {
+        var latLng = new google.maps.LatLng(data.lat, data.lng); 
+        // Creating a marker and putting it on the map
+        var marker = new google.maps.Marker({
+            position: latLng,
+            title: data.title
+        });
+        marker.setMap(map);
+      });
+    });
+        
+    /*
+    // Create a marker and set its position.
+    var marker = new google.maps.Marker({
+      map: map,
+      position: myLatLng,
+      animation: google.maps.Animation.DROP,
+      title: 'Hello World!'
+    });
+    */
+}
+                  
 (function($) {
     "use strict"; // Start of use strict
 
@@ -18,7 +50,9 @@
         offset: 51
     });
 
-    // Closes the Responsive Menu on Menu Item Click
+    $('[data-toggle="tooltip"]').tooltip();
+    
+/*    // Closes the Responsive Menu on Menu Item Click
     $('.navbar-collapse ul li a').click(function(){ 
             $('.navbar-toggle:visible').click();
     });
@@ -35,5 +69,5 @@
             top: 100
         }
     })
-
+*/
 })(jQuery); // End of use strict
