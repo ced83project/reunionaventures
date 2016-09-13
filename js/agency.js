794949c914d9a453d9946b9610638a75f3e849f4
Cedric <c.beyls@gmail.com>
2016-09-14 01:14:47 +0200

    Add map and carousel

--- js/agency.js
+++ js/agency.js
@@ -6,19 +6,47 @@ function initMap() {
     var map = new google.maps.Map(document.getElementById('map'), {
       center: {lat: -21.115141, lng: 55.536384},
       scrollwheel: false,
-      zoom: 10 
+      zoom: 11 
     });
     
-    $.getJSON("locations.json", function(json1) {
+    $.getJSON("/locations.json", function(json1) {
       $.each(json1, function(key, data) {
-        var latLng = new google.maps.LatLng(data.lat, data.lng); 
+      
+        // Create the latLng
+        var latLng = new google.maps.LatLng(data.lat, data.lng);         
+      
+        // Creating the icon
+        var pinColor = data.color;
+        var pinLetter = data.letter;
+        var pinImage = new google.maps.MarkerImage("http://chart.apis.google.com/chart?chst=d_map_pin_letter&chld=" + pinLetter + "|" + pinColor,
+          new google.maps.Size(21, 34),
+          new google.maps.Point(0,0),
+          new google.maps.Point(10, 34));
+        
         // Creating a marker and putting it on the map
         var marker = new google.maps.Marker({
             position: latLng,
-            title: data.title
+            title: data.title,
+            icon: pinImage
         });
+        
+        // Creating the info Windows
+        var contentString = '<span>'+ data.name +'</span>';
+        var infowindow = new google.maps.InfoWindow({
+          content: contentString
+        });
+        
+        // Add the infoWindow listener
+        marker.addListener('click', function() {
+          infowindow.open(map, marker);
+        });
+        
+        // Add the marker to the map
         marker.setMap(map);
+        
       });
+    }).error(function(jqXhr, textStatus, error) {
+        console.log("ERROR: " + textStatus + ", " + error);
     });
         
     /*
